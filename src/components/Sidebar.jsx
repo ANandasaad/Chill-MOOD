059a872161dfad78c9711f85e04c35293f52f8d4
Anand Kushwaha <98548977+ANandasaad@users.noreply.github.com>
2023-04-08 03:42:28 +0530

    sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,22 +4,29 @@ import { RiCloseLine } from "react-icons/ri";
 import { logo } from "../assets";
 import { links } from "../assets/constants";
 
-// const NavLink =()=>{
+const Sidebar = ({handleClick}) => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  console.log(links[0].name);
 
-//   <div className="">
-
-//   </div>
-
-// }
-
-const Sidebar = () => {
- const [mobileMenuOpen, setMobileMenuOpen]= useState(false);
-
-
-  return <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#070524]">
-     <img src={logo} alt="logo" className="w-full h-14 object-contain"/>
-     {/* <NavLink/> */}
-  </div>;
+  return (
+    <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#042221]">
+      <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+      {links.map((item) => (
+        <li
+          key={item.name}
+          className="list-none mt-10"
+        >
+          <NavLink className=" flex justify-start items-center my-2 font-medium text-sm text-gray-400 hover:text-cyan-400"
+          to={item.to}
+          onClick={()=> handleClick && handleClick()}
+          >
+            <item.icon className="w-6 h-6 mr-2"/>
+            {item.name}
+          </NavLink>
+        </li>
+      ))}
+    </div>
+  );
 };
 
 export default Sidebar;
