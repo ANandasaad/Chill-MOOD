1f7b4dede7880a7cc3073bedb7136c46981330e2
Anand Kushwaha <98548977+ANandasaad@users.noreply.github.com>
2023-04-08 02:58:54 +0530

    color

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,13 +3,22 @@ import { NavLink } from "react-router-dom";
 import { RiCloseLine } from "react-icons/ri";
 import { logo } from "../assets";
 import { links } from "../assets/constants";
+
+// const NavLink =()=>{
+
+//   <div className="">
+
+//   </div>
+
+// }
+
 const Sidebar = () => {
  const [mobileMenuOpen, setMobileMenuOpen]= useState(false);
 
 
-  return <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
+  return <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#070524]">
      <img src={logo} alt="logo" className="w-full h-14 object-contain"/>
-
+     {/* <NavLink/> */}
   </div>;
 };
 
